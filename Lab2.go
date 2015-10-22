f65e70d0fa8b6776181b2da5eb273849dd43fa1e
dhavalgangar <dhaval.gangar@yahoo.com>
2015-10-21 20:22:11 -0700

    Dhaval Gangar CMPE273 LAB2

---
+++ Lab2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"httprouter"
+	"net/http"
+)
+
+var string1 string
+
+// Req represents the structure of request
+type Req struct {
+	Name string `json:"name"`
+}
+
+// Resp represents the structure of request
+type Resp struct {
+	Greeting string `json:"greeting"`
+}
+
+// GreetUser creates a new user resource
+func GreetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	u := Resp{}
+	u2 := Req{}
+	json.NewDecoder(r.Body).Decode(&u2)
+
+	var buffer bytes.Buffer
+	buffer.WriteString("Hello, ")
+	buffer.WriteString(u2.Name)
+	buffer.WriteString("!")
+	u.Greeting = buffer.String()
+
+	// Marshal provided interface into JSON structure
+	uj, _ := json.Marshal(u)
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+func main() {
+	// Instantiate a new router
+	r := httprouter.New()
+
+	r.POST("/user", GreetUser)
+
+	http.ListenAndServe("localhost:3000", r)
+}
